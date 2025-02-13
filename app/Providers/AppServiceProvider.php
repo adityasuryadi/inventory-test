130b3fc4c9ec4d0645b03fc508a842e4267e8d8f
Aditya Suryadi <adityasuryaditya@gmail.com>
2025-02-13 09:54:23 +0700

    feat:add history update score

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use App\Services\ProductService;
-use App\Services\Impl\ProductServiceImpl;
+use App\Events\ScoreUpdated;
+use App\Listeners\updateScoreHistoryListener;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
